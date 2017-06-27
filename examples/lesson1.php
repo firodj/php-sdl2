964c0cd903f3ecd0b78bbf97bd4531ef10253a7d
Fadhil Mandaga <firodj@gmail.com>
2017-06-27 17:46:09 +0700

    EXAMPLE: fix after TimerEvent

--- examples/lesson1.php
+++ examples/lesson1.php
@@ -6,41 +6,27 @@ SDL\initIMG(SDL\INIT_PNG);
 $window = new SDL\Window("Wew", SDL\WindowPos::UNDEFINED, SDL\WindowPos::UNDEFINED, 800, 600, SDL\WindowFlags::SHOWN);
 $renderer = new SDL\Renderer($window, -1, SDL\RendererFlags::ACCELERATED);
 
-/*$sprite = new SDL\Texture($renderer, SDL\PixelFormat::ARGB8888,
-    SDL\TextureAccess::STATIC, 16, 16);
- */
+// $sprite = new SDL\Texture($renderer, SDL\PixelFormat::ARGB8888, SDL\TextureAccess::STATIC, 16, 16);
 
 $renderer->setDrawColor(0x00, 0x00, 0xff);
 
-class Waktu extends SDL\Timer
-{
-    private $renderer;
-    public $pos;
-
-    public function __construct($renderer) {
-        $this->renderer = $renderer;
-
-        $surface = SDL\Surface::loadFromFile(__DIR__.'/RyuHayabusa.png');
-        $surface->setColorKey($surface->mapRGB(0xc8, 0xa8, 0xb8), true);
-        $this->sprite = SDL\Texture::createFromSurface($renderer, $surface);
-
-        $this->rect = new SDL\Rect(22, 26, 17, 32);
-        $this->pos = new SDL\Rect(0, 0, 17, 32);
-    }
-
-    public function run(int $timestamp=null, int $interval=null) {
-        $this->renderer->clear();
-        $this->renderer->copy($this->sprite, $this->rect, $this->pos, 45);
-        $this->renderer->present();
-
-        //printf("(got:%d,%d)", $interval, $timestamp);
-    }
-}
-
 SDL\Event::setState(SDL\EventType::FINGERMOTION, false);
 
-$timer = new Waktu($renderer);
-$timer->start(33);
+$surface = SDL\Surface::loadFromFile(__DIR__.'/RyuHayabusa.png');
+$surface->setColorKey($surface->mapRGB(0xc8, 0xa8, 0xb8), true);
+$sprite = SDL\Texture::createFromSurface($renderer, $surface);
+$rect = new SDL\Rect(22, 26, 17, 32);
+$pos = new SDL\Rect(0, 0, 17, 32);
+
+function refresh(int $timestamp=null, int $interval=null) {
+    global $pos, $sprite, $rect, $renderer;
+    $renderer->clear();
+    $renderer->copy($sprite, $rect, $pos, 45);
+    $renderer->present();
+}
+
+$timer = SDL\Timer::start(33);
+printf("ID:%d\n", $timer->id);
 
 $quit = false;
 do {
@@ -56,8 +42,8 @@ do {
             $quit = true;
             break;
         case SDL\EventType::MOUSEMOTION:
-            $timer->pos->x = $e->x;
-            $timer->pos->y = $e->y;
+            $pos->x = $e->x;
+            $pos->y = $e->y;
             break;
         case SDL\EventType::MOUSEBUTTONDOWN:
             printf("buttondown.%d(%d,%d)", $e->button, $e->x, $e->y);
@@ -67,19 +53,24 @@ do {
             break;
         case SDL\EventType::WINDOWEVENT:
             if ($e->event == SDL\WindowEvent::CLOSE) {
-                echo "Quit";
-                $quit = true;
+                SDL\Event::push(new SDL\Event(SDL\EventType::QUIT));
             }
             printf("windowevent.%d", $e->event);
             break;
+        case SDL\EventType::TIMEREVENT:
+            refresh($e->timestamp, $e->interval);
+            break;
         default:
-            //printf("[type:%d]", $e->type);
         }
+        unset($e);
     }
 } while (!$quit);
 
-$timer->stop();
-echo "stop:".SDL\Timer::getTicks().PHP_EOL;
+SDL\Timer::stop($timer);
+unset($timer);
+
+unset($renderer);
+unset($window);
 
 SDL\quitIMG();
 SDL\quit();
